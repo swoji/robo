1623aacfd20787d8369340caa4fce5f60260c3b1
swoji <davidookuku@gmail.com>
2023-12-29 17:11:17 +0300

    Add specific operations for app

---
+++ actions/update-list/index.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
+import  { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { createAuditLog } from "@/lib/create-audit-log";
+
+import { UpdateList } from "./schema";
+import { InputType, ReturnType } from "./types";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unathorized",
+    };
+  }
+
+  const { title, id, boardId } = data;
+  let list;
+
+  try {
+    list = await db.list.update({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+      data: {
+        title,
+      },
+    });
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.UPDATE,
+    })
+  } catch (error) {
+    return {
+      error: "Failed to update."
+    }
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const updateList = createSafeAction(UpdateList, handler);
